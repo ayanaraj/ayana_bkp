8a41516a9aa3b358dd3e0aa513249de2876053c9
Ayana Raj <100473207+ayanaraj@users.noreply.github.com>
2022-03-06 21:55:24 +0530

    Add files via upload

---
+++ order-processing/order-processing/src/main/java/com/gbs/orderprocessing/Controller/OrderDTO.java
@@ -0,0 +1,23 @@
+package com.gbs.orderprocessing.Controller;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+
+
+    private List<Integer> items;
+
+    private Double netPrice;
+
+    private String feedback;
+
+}
---
+++ order-processing/order-processing/src/main/java/com/gbs/orderprocessing/Model/Order.java
@@ -0,0 +1,44 @@
+package com.gbs.orderprocessing.Model;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "Orders")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
+    @JsonIgnoreProperties("order") // Ignore this property when converting to JSON
+    private List<Item> items;
+
+
+
+    @Column(name = "feedback")
+    private String feedback;
+
+    @Column(name = "net_price")
+    private Double netPrice;
+
+}
